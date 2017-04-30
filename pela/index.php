14d5a1c606a2c3a7e051b6f42361bf9bff62bff0
Tehnikumi Õpilane <opilane@tptlive.ee>
2017-04-30 15:33:46 +0300

    rightLavaJ

--- pela/index.php
+++ pela/index.php
@@ -182,7 +182,7 @@ session_start();
 			console.log(element.hasClass('water'));
 
 			// Liigume ainult siis, kui ei ole vesi
-			if (!element.hasClass("water")) {            
+			if (!element.hasClass("water") && !element.hasClass("lava")) {            
             setUserLocation(user_x + 1, user_y);
             }
         }
